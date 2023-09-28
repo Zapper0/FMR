fc83755458e721b4d7d0eb8dfe54c4902391724c
zap <rmul.maciel@gmail.com>
2023-09-28 12:08:22 -0300

    comments

--- controle/controle.ino
+++ controle/controle.ino
@@ -5,34 +5,36 @@ int ledPin = 10;
 int leftButton = 7;
 int rightButton = 6;
 
-RH_ASK driver;
+RH_ASK driver; // init radio driver
 
 void setup()
 {
-    Serial.begin(115200); // Debugging only
+    Serial.begin(115200);
+
     pinMode(ledPin, OUTPUT);
     pinMode(leftButton, INPUT_PULLUP);
     pinMode(rightButton, INPUT_PULLUP);
+
     if (!driver.init())
-        Serial.println("init failed");
+        Serial.println("init failed"); // Make sure radio is working
 }
 
 void loop()
 {
     if (digitalRead(leftButton) == LOW)
     {
-        digitalWrite(ledPin, HIGH);
-        driver.send((uint8_t *)"l", 1);
-        driver.waitPacketSent();
+        digitalWrite(ledPin, HIGH);     // Turn on a light to show transmitting
+        driver.send((uint8_t *)"l", 1); // Send a message
+        driver.waitPacketSent();        // Wait for message to be sent
         delay(100);
-        digitalWrite(ledPin, LOW);
+        digitalWrite(ledPin, LOW); // Turn off the light to show end of transmission
     }
     if (digitalRead(rightButton) == LOW)
     {
-        digitalWrite(ledPin, HIGH);
-        driver.send((uint8_t *)"r", 1);
-        driver.waitPacketSent();
+        digitalWrite(ledPin, HIGH);     // Turn on a light to show transmitting
+        driver.send((uint8_t *)"r", 1); // Send a message
+        driver.waitPacketSent();        // Wait for message to be sent
         delay(100);
-        digitalWrite(ledPin, LOW);
+        digitalWrite(ledPin, LOW); // Turn off the light to show end of transmission
     }
 }
--- robo/robo.ino
+++ robo/robo.ino
@@ -9,14 +9,18 @@ RH_ASK driver;
 
 void setup()
 {
-  Serial.begin(115200); // Debugging only
+  Serial.begin(115200);
+
   pinMode(ledPin, OUTPUT);
   pinMode(lMotor, OUTPUT);
   pinMode(rMotor, OUTPUT);
+
+  // Set relays off
   digitalWrite(lMotor, HIGH);
   digitalWrite(rMotor, HIGH);
+
   if (!driver.init())
-    Serial.println("init failed");
+    Serial.println("init failed"); // Make sure radio is working
 }
 
 void loop()
@@ -25,25 +29,21 @@ void loop()
   uint8_t buflen = sizeof(buf);
 
   // blink the led when a message is received
-  if (driver.recv(buf, &buflen)) // Non-blocking
+  if (driver.recv(buf, &buflen)) // Check if received packet is correct size
   {
     int i;
-    // Message with a good checksum received, dump it.
-    // Serial.print("Message: ");
-    // Serial.println((char *)buf);
+    // Message with a good checksum received, proceed
 
+    // Blink led
     digitalWrite(ledPin, HIGH);
     delay(100);
     digitalWrite(ledPin, LOW);
 
-    Serial.print((char *) buf);
+    // Print received message (debug)
+    Serial.print((char *)buf);
     Serial.print(" | ");
     Serial.print(String((char *)buf == "l"));
     Serial.print(" | ");
     Serial.println(strcmp((char *)buf, "r"));
-    if((char *) buf == 'l')
-    {
-      Serial.println("left");
-    }
-    }
+  }
 }
